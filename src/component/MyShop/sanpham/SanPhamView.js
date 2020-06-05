9ffd8a9173bd77cba50dc3af4546b25c16a6129f
trieule47 <62042242+trieule47@users.noreply.github.com>
2020-06-05 22:10:54 +0700

    up anh len sv

--- src/component/MyShop/sanpham/SanPhamView.js
+++ src/component/MyShop/sanpham/SanPhamView.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
 import {
-  View,
-  Text,
-  Dimensions,
-  Image,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-  ActivityIndicator,
-  Alert,
+    View,
+    Text,
+    Dimensions,
+    Image,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
+    ActivityIndicator,
+    Alert,
+    Button,
 } from "react-native";
 import Header from "../Header";
 import Detail from "../sanpham/Detail";
 import { connect } from "react-redux";
 import {
-  actAllInfoShopRequest
+    actAllInfoShopRequest
 } from "../../../action/ShopAction";
 
 import Colection from "../../Main/Shop/Home/Category";
@@ -22,63 +23,67 @@ import Colection from "../../Main/Shop/Home/Category";
 const url = "http://vaomua.club/public/user/image/images/";
 
 class SanPham extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      isLoading: true,
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+        }
+
     }
-    
-  }
-  componentDidMount() {
-    
-    this.props.GetInforShop(this.props.user.infoUser.id);
-    console.log("ALO LAO " + this.props.user.infoUser.id);
-  }
-  
+    componentDidMount() {
 
-  render() {
-    const { myshop, user } = this.props;
-    const {
-      container,
-      titleContainer,
-      title,
-      body,
-      productContainer,
-      productImage,
-      productName,
-      productPrice,
-    } = styles;
+        this.props.GetInforShop(this.props.user.infoUser.id);
+        console.log("ALO LAO " + this.props.user.infoUser.id);
+    }
 
-    
-    return (
-      <ScrollView>
-        <Header a={myshop.inforShop}  navigation={this.props.navigation}/>
-        <Colection/>
-        <View style={container}>
-          <View style={titleContainer}>
-            <Text style={title}>My PRODUCT</Text>
-          </View>
-          <Detail b={myshop.sanphamshop} navigation={this.props.navigation} />
-      </View>
-      <TouchableOpacity onPress={()=> Alert.alert(JSON.stringify(myshop.inforShop))} >
-          <Text> CC</Text>
-      </TouchableOpacity>
-      </ScrollView>
-    );
-  }
+
+    render() {
+        const { myshop, user } = this.props;
+        const {
+            container,
+            titleContainer,
+            title,
+            btnThem1,
+            btnThem,
+        } = styles;
+
+        const { navigation } = this.props;
+        return (
+            <View>
+                <Header a={myshop.inforShop} navigation={this.props.navigation} />
+                <ScrollView>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <TouchableOpacity style={btnThem} onPress={()=> navigation.navigate('AddProduct')}>
+                        <Text style={{color: '#FFF'}}>Thêm sản phẩm</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={btnThem1} onPress={()=> navigation.navigate('ThongTinShop')}>
+                        <Text style={{color: '#FFF'}}>Thông tin shop</Text>
+                    </TouchableOpacity>
+                </View>
+                <Colection />
+                <View style={container}>
+                    <View style={titleContainer}>
+                        <Text style={title}>My PRODUCT</Text>
+                    </View>
+                    <Detail b={myshop.sanphamshop} navigation={this.props.navigation} />
+                </View>
+                </ScrollView>
+            </View>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    myshop: state.myshop,
-    user: state.user,
-  };
+    return {
+        myshop: state.myshop,
+        user: state.user,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    GetInforShop: (id_user) => dispatch(actAllInfoShopRequest(id_user)),
-  };
+    return {
+        GetInforShop: (id_user) => dispatch(actAllInfoShopRequest(id_user)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SanPham);
@@ -88,48 +93,78 @@ const productWidth = (width - 60) / 2;
 const productImageHeight = (productWidth / 361) * 425;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#FFF",
-    margin: 5,
-    shadowColor: "#2E272B",
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.2,
-  },
-  titleContainer: {
-    height: 50,
-    justifyContent: "center",
-    paddingLeft: 10,
-  },
-  title: {
-    color: "#D3D3CF",
-    fontSize: 20,
-  },
-  body: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    flexWrap: "wrap",
-  },
-  productContainer: {
-    width: productWidth,
-    shadowColor: "#2E272B",
-    shadowOffset: { width: 0, height: 3 },
-    shadowOpacity: 0.2,
-    paddingBottom: 10,
-  },
-  productImage: {
-    width: productWidth,
-    height: productImageHeight,
-  },
-  productName: {
-    paddingLeft: 10,
+    container: {
+        backgroundColor: "#FFF",
+        margin: 5,
+        shadowColor: "#2E272B",
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+    },
+    titleContainer: {
+        height: 50,
+        justifyContent: "center",
+        paddingLeft: 10,
+    },
+    title: {
+        color: "#D3D3CF",
+        fontSize: 20,
+    },
+    body: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+    },
+    productContainer: {
+        width: productWidth,
+        shadowColor: "#2E272B",
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        paddingBottom: 10,
+    },
+    productImage: {
+        width: productWidth,
+        height: productImageHeight,
+    },
+    productName: {
+        paddingLeft: 10,
 
-    color: "#D3D3CF",
-    fontWeight: "500",
-    marginTop: 5,
-  },
-  productPrice: {
-    paddingLeft: 10,
+        color: "#D3D3CF",
+        fontWeight: "500",
+        marginTop: 5,
+    },
+    productPrice: {
+        paddingLeft: 10,
 
-    color: "#662F90",
-  },
+        color: "#662F90",
+    },
+    btnThem :{
+        height: 50,
+        width: 200,
+        borderWidth: 1,
+        borderRadius: 5,
+        textAlign: 'justify',
+        alignContent: 'space-between',
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        flex: 1,
+        paddingLeft: 45,
+        backgroundColor: '#2CBE4E',
+        borderColor: '#FFF',
+        marginLeft: 4
+    },
+    btnThem1:{
+        height: 50,
+        width: 200,
+        borderWidth: 1,
+        borderRadius: 5,
+        textAlign: 'justify',
+        alignContent: 'space-between',
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        flex: 1,
+        paddingLeft: 45,
+        backgroundColor: '#2CBE4E',
+        borderColor: '#FFF',
+        marginRight: 3,
+    },  
 });
